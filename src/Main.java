8d3f6b1ebc39bb595ebd41f01db9e8f744df039d
Miguel da Silva Bassi <migueldsbassi@gmail.com>
2026-09-04 10:10:36 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
-void main() {
-    System.out.println("Sistema de projetos");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sistema de projetos");
+    }
 }
